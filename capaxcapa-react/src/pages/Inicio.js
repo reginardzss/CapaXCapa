19a710a1c57b3c8436c62a3fc553aadc93de78af
Juan Daniel Rodríguez Oropeza <j_danny07@hotmail.com>
2022-07-09 12:06:51 -0500

    Fuentes de escritura agregadas
    Se tuvieron que instalar y usar mediante Font Face porque dichas fuentes no se encuentran en Google Fonts.

---
+++ capaxcapa-react/src/pages/Inicio.js
@@ -0,0 +1,11 @@
+import "../App.css"
+
+function Inicio(){
+    return (
+        <div className="font-face-ananias">
+            <h1>Inicio</h1>
+        </div>
+    )
+}
+
+export default Inicio
---
+++ capaxcapa-react/src/pages/Modelos.js
@@ -0,0 +1,9 @@
+function Modelos(){
+    return (
+        <div>
+            <h1>Modelos</h1>
+        </div>
+    )
+}
+
+export default Modelos
