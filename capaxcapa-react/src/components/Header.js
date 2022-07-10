4285076f7583afc5191ee9663e1b4c1dbaa057ae
Juan Daniel Rodríguez Oropeza <j_danny07@hotmail.com>
2022-07-10 03:03:32 -0500

    Avance 2 Header
    Se corrigieron los detalles anteriores y ya quedaron los elementos acomodados. Estaría bien implementar que cambie el color de las letras cuando se pasa el cursor sobre este.

--- capaxcapa-react/src/components/Header.js
+++ capaxcapa-react/src/components/Header.js
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom"
 import Button from "./Button"
-import ButtonMailto from "./ButtonMailto";
 
 import React, { useEffect, useState} from 'react';
 
@@ -8,21 +7,21 @@ import {BsInstagram} from 'react-icons/bs'
 import {TbMail} from 'react-icons/tb'
 
 function Header(){
+    
+
     return(
         
-    <div className="bg-primary d-flex">
+    <div className="bg-primary d-flex" style={{height: '50px'}}>
         <div className="p-2 font-face-cerebri-sans-book">
-            <img src={require("../Images/Icons/Logo-CapaXCapa1.png")} width="100" height="50"/>
-            <Link to={"/inicio"}> <Button color={"primary"}>Inicio</Button> </Link>
+            <img src={require("../Images/Icons/Logo-CapaXCapa1.png")} height="40"/>
+            <Link to={"/inicio"}> <Button color={"primary"}> Inicio</Button> </Link>
             <Link to={"/nosotros"}> <Button color={"primary"}>Nosotros</Button> </Link>
             <Link to={"/modelos"}> <Button color={"primary"}>Modelos</Button> </Link>
             <Link to={"/contacto"}> <Button color={"primary"}>Contacto</Button> </Link>
         </div>
-        <div className="p-2 flex-fill">
-            <div className="d-flex justify-content-end">
-                <ButtonMailto label={<TbMail size="40px" />} mailto="mailto:capaxcapamty@gmail.com" />
-                <Button onClick={event => window.location.href='https://www.instagram.com/capaxcapa_oficial/'} color={"primary"}> <BsInstagram size="40px"/> </Button>
-            </div>
+        <div className="d-flex justify-content-end flex-fill">
+            <Button onClick={() => window.location = 'mailto:capaxcapamty@gmail.com'}> <TbMail size="40px"/> </Button>
+            <Button onClick={event => window.location.href='https://www.instagram.com/capaxcapa_oficial/'} color={"primary"}> <BsInstagram size="30px"/> </Button>
         </div>
     </div>
     )
