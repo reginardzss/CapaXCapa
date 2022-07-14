2db22adb52aba3209860d654be991126abb11b0e
MarioR4 <a01197707@itesm.mx>
2022-07-13 21:25:26 -0500

    Comentarios
    Comentarios en App.js

--- capaxcapa-react/src/App.js
+++ capaxcapa-react/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+// Importar aqui los componentes de las páginas que se vayan creando
 import Inicio from './components/pages/Inicio/Inicio';
 import Nosotros from './components/pages/Nosotros/Nosotros';
 import Modelos from './components/pages/Modelos/Modelos';
@@ -17,10 +18,11 @@ function App() { // esta función maneja el ciclo de vida de la aplicación.
         <Header /> {/* header de navegación constante en todas las páginas */}
         <Routes>
           <Route path='' element={<Navigate to="/inicio" replace={true} />}/>    {/* la página default será Home, Navigate sirve para redireccionar */}
-          <Route path='/inicio' element={<Inicio/>} />
-          <Route path='/nosotros' element={<Nosotros/>} />
-          <Route path='/modelos' element={<Modelos/>} />
-          <Route path='/contacto' element={<Contacto/>} />
+          <Route path='/inicio' element={<Inicio />} />
+          <Route path='/nosotros' element={<Nosotros />} />
+          <Route path='/modelos' element={<Modelos />} />
+          <Route path='/contacto' element={<Contacto />} />
+          {/* Agregar aqui la ruta a los componentes que se vayan importando.   /inicio es como aparecerá en el URL de la página */}
         </Routes>
         <footer><Footer /></footer>
       </BrowserRouter>
