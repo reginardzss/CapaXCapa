aa7807ac1021fab2fd74630a23be8e1b634e66c8
MarioR4 <a01197707@itesm.mx>
2022-07-14 16:23:11 -0500

    Galeria Slideshow 60%
    Faltan los botones y le logo de CxC en las imagenes de la galería

--- capaxcapa-react/src/components/pages/Inicio/Galeria.js
+++ capaxcapa-react/src/components/pages/Inicio/Galeria.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Link } from "react-router-dom"
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
+
 import './Inicio.css';
 
 import FotoSlide1 from '../../../media/Images/Inicio-slide-1.jpg';
@@ -15,25 +17,49 @@ const Galeria = () => {
                 <div style={{ 'backgroundImage': `url(${FotoSlide1})` }}>
                     <span>
                         <div className="slideshow-container">
+                            <div style={{ 'height': `120px` }}></div>
                             <div>"Si no estas dispuesto a aprender, nadie te puede ayudar</div>
+                            <div style={{ 'height': `60px` }}></div>
                             <div>Si estas dispuesto a aprender, nadie te puede parar"</div>
+                            <div style={{ 'height': `60px` }}></div>
                         </div>
                     </span>
                 </div>
             </div>
             <div className="each-slide-effect">
                 <div style={{ 'backgroundImage': `url(${FotoSlide2})` }}>
-                    <span>Slide 2</span>
+                    <span>
+                        <div className="slideshow-container">
+                            <div style={{ 'height': `120px` }}></div>
+                            <div>"Si no estas dispuesto a aprender, nadie te puede ayudar</div>
+                            <div style={{ 'height': `60px` }}></div>
+                            <div>Si estas dispuesto a aprender, nadie te puede parar"</div>
+                        </div>
+                    </span>
                 </div>
             </div>
             <div className="each-slide-effect">
                 <div style={{ 'backgroundImage': `url(${FotoSlide3})` }}>
-                    <span>Slide 3</span>
+                    <span>
+                        <div className="slideshow-container">
+                            <div style={{ 'height': `120px` }}></div>
+                            <div>"Si no estas dispuesto a aprender, nadie te puede ayudar</div>
+                            <div style={{ 'height': `60px` }}></div>
+                            <div>Si estas dispuesto a aprender, nadie te puede parar"</div>
+                        </div>
+                    </span>
                 </div>
             </div>
             <div className="each-slide-effect">
                 <div style={{ 'backgroundImage': `url(${FotoSlide4})` }}>
-                    <span>Slide 3</span>
+                    <span>
+                        <div className="slideshow-container">
+                            <div style={{ 'height': `120px` }}></div>
+                            <div>"Si no estas dispuesto a aprender, nadie te puede ayudar</div>
+                            <div style={{ 'height': `60px` }}></div>
+                            <div>Si estas dispuesto a aprender, nadie te puede parar"</div>
+                        </div>
+                    </span>
                 </div>
             </div>
         </Slide>
