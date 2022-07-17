33c1f146b651debff1c3a682da532cb44821ebfd
Juan Daniel Rodríguez Oropeza <j_danny07@hotmail.com>
2022-07-16 19:41:58 -0500

    Header y Footer terminados.
    Ya se terminaron de desarrollar los Header y Footer, aunque aun queda la duda de si hay vistas para los textos de abajo. También se encontró una incroguencia en el Figma con respecto al diseño del Footer, ya que cambia en algunas vistas la parte de "Síguenos".

--- capaxcapa-react/src/components/Footer.js
+++ capaxcapa-react/src/components/Footer.js
@@ -1,11 +1,37 @@
 import { Link } from "react-router-dom"
 import Button from "./Button"
 
+import React from 'react';
+
+import {BsInstagram} from 'react-icons/bs'
+
+import "../assets/scss/app.scss"
+import "../App.css"
+import "./stylesCompartidas/Footer.css"
+
 function Footer(){
     return(
-    <div className="p-2 bg-secondary fst-italic">
-        <p className="text-center">Toda propiedad intelectual es compartida a Capa X Capa.</p>
-    </div>
+        <div className="bg-danger d-flex" style={{height: '120px', alignItems: 'center'}}>
+            <div className="p-2">
+                <img src={require("../media/Images/Icons/Logo-CapaXCapa1.png")} height="40"/>
+            </div>
+            <div className="font-face-ananias" style={{marginLeft: '15px'}}>
+                <h1> <span className="Capa">CAPA</span> <span className="X">X</span> <span className="Capa">CAPA</span> </h1>
+            </div>
+            <div className="p-2 justify-content-end d-flex flex-fill font-face-cerebri-sans-book" style={{marginTop: '10px'}}>
+                <div className="p-2 flex-fill justify-content-end" style={{marginLeft: '250px'}}>
+                    <h2 className="TextoFooter">¡Síguenos!</h2>
+                    <Button onClick={event => window.location.href='https://www.instagram.com/capaxcapa_oficial/'}> <BsInstagram size="30px" color="E4C76A"/> </Button>
+                </div>
+                <div className="p-2 TextoFooter flex-fill" style={{textAlign: "left", marginLeft: '70px'}}>
+                    <h2>Derechos de autor</h2>
+                    <h2>Términos Y Condiciones</h2>
+                </div>
+                <div className="p-2 TextoFooter" style={{textAlign: "left", marginRight: '50px'}}>
+                    <h2>Aviso de Privacidad</h2>
+                </div>
+            </div>
+        </div>
     )
 }
 
