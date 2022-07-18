06441999f4b45a75b0760713dd0517b7fddeab9f
Fernando Fabrizzio Ramírez <fernandofabrizzioramirez@gmail.com>
2022-07-18 18:57:34 -0500

    Imagen de fondo en view
    Se importa componente a la view, no se configura ni se introducen parámetros (componente imagen Fondo tiene texto fijo, es necesario introducir parámetro, pendiente por verificar con responsable).

--- capaxcapa-react/src/components/pages/QueHacemos/QueHacemos.js
+++ capaxcapa-react/src/components/pages/QueHacemos/QueHacemos.js
@@ -1,6 +1,8 @@
+import ImagenFondo from '../../ImagenFondo.js'
 function QueHacemos(){
     return (
         <div>
+            <ImagenFondo/>
             <h1>¿Qué Hacemos?</h1>
         </div>
     )
