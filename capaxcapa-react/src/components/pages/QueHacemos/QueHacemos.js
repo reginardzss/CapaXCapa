8a03d11fb93c8ded52dc476e44785bd9aaa823a6
Fernando Fabrizzio Ramírez <fernandofabrizzioramirez@gmail.com>
2022-07-19 19:02:06 -0500

    Props Título de fondo principal.
    Se implementa el texto del fondo principal con props.

--- capaxcapa-react/src/components/pages/QueHacemos/QueHacemos.js
+++ capaxcapa-react/src/components/pages/QueHacemos/QueHacemos.js
@@ -3,7 +3,7 @@ import fondoSlide from '../../../media/Images/Inicio-slide-3.jpg'
 function QueHacemos(){
     return (
         <div>
-            <ImagenFondo imagen={fondoSlide}/>
+            <ImagenFondo imagen={fondoSlide} titulo={"¿Qué hacemos?"}/>
             <h1>¿Qué Hacemos?</h1>
         </div>
     )
