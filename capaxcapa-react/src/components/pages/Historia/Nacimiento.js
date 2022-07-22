391794ee71bd8dd6cb4796574bc5ff0ca7f6a32b
mbenitezm01 <103158739+mbenitezm01@users.noreply.github.com>
2022-07-22 12:37:52 -0400

    Avance en Nuestra Historia

--- capaxcapa-react/src/components/pages/Historia/Nacimiento.js
+++ capaxcapa-react/src/components/pages/Historia/Nacimiento.js
@@ -9,6 +9,18 @@ const Nacimiento = () => {
                 Capa x Capa nace de un proyecto escolar que buscaba <b>mantener la motivación de los estudiantes</b> por la visible falta de motivación a causa de la pandemia.
                 </p>
             </div>
+
+            <div className="nacimiento-contenido2">
+                <p>
+                Se sumaron todas las <b className='b2'>habilidades</b> del equipo, llenando <b className='b2'>cada casilla</b> y <b className='b2'>complementándonos</b> para hacer realidad esta iniciativa.
+                </p>
+            </div>
+            
+            <div className="nacimiento-contenido3">
+                <p>
+                Nuestra batería fue el motivar a alumno por alumno a no dejar de prepararse para <b className='b3'>alcanzar su mayor potencial.</b>
+                </p>
+            </div>
         </div>
     )
 }
