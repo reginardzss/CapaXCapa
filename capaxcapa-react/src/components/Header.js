6e5c8f959fe24f53fe84e1f220b3693ae1cca3bd
Juan Daniel Rodríguez Oropeza <j_danny07@hotmail.com>
2022-07-24 19:53:42 -0500

    Terminado Vista de "¿Quiénes Somos?" y otras correcciones
    1.- Se terminó con la vista de "¿Quiénes Somos?".
    2.- Las imágenes del header y footer se reemplazaron con svg. Antes eran png.
    3.- En el header y footer ya no se utiliza componente Button.js, ahora se utiliza el elemento HTML "button" y se estilizó con CSS, haciendo que sea más ligero el proyecto y que se pueda modificar el tamaño de la fuente de los botones.
    Lo que ahora significa que el Header es más fiel al diseño de Figma.
    4.- Se eliminaron del repositorio imágenes png innecesarias, y se reemplazaron por su contraparte en svg.
    5.- En la vista de "¿Quiénes Somos?" anteriormente se había utilizado la imagen svg de las manos y las palabras estaban hechas con texto y cada letra se rotaba y se ponía en su posición. Ahora esto se reemplazó por un recurso gráfico, que es una imagen svg que contiene todo. Esto con el objetivo de que este contenido pueda ser responsivo más fácilmente.
    6.- Se agregó un nuevo archivo css llamado "Botones-Iconos-css" en la carpeta de stylesCompartidas para que puedan hacer sus botones transaparente, tomando en cuenta que se utilice la etiqueta HTML de "button".
    7.- El css de "Línea" ya no viene con márgenes por defecto, permitiendo ahora que en cada vista individual se puedan personalizar los márgenes de top y bottom.
    8.- En la carpeta de "elementos-compartidos" ahora hay dos versiones del Logo de Capa X Capa, una con texto y otra sin texto.

--- capaxcapa-react/src/components/Header.js
+++ capaxcapa-react/src/components/Header.js
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom"
-import Button from "./Button"
 
 import React from 'react';
 
@@ -7,21 +6,28 @@ import React from 'react';
 import {BsInstagram} from 'react-icons/bs' // Ícono de Instagram
 import {TbMail} from 'react-icons/tb' // Ícono de e-mail
 
+// Importación de imágenes
+import LogoCapaXCapa from '../media/Images/Icons/elementos-gráficos/LogoCapaXCapa-SinTexto.svg'
+
+// Importación de estilos
+import './stylesCompartidas/Header.css'
+import './stylesCompartidas/Botones-Iconos.css'
+
 function Header(){
     return(
         <div className="bg-primary" style={{height: '60px', width: '100%', display: 'flex'}}>
             {/* Botones que redireccionan a otras partes de la web. También se encuentra el logo de Capa X Capa. Están a la izquierda respectivamente. */}
-            <div className="font-face-cerebri-sans-pro">
-                <img src={require("../media/Images/Icons/Logo-CapaXCapa2.png")} style={{height: '2.986vw', width: '3.68vw', margin: '8px 2.986vw 9px 4.166vw'}}/>
-                <Link to={"/inicio"}> <Button color={"primary"}>Inicio</Button> </Link>
-                <Link to={"/nosotros"}> <Button color={"primary"}>Nosotros</Button> </Link>
-                <Link to={"/modelos"}> <Button color={"primary"}>Modelos</Button> </Link>
-                <Link to={"/contacto"}> <Button color={"primary"}>Contacto</Button> </Link>
+            <div className="font-face-cerebri-sans-pro" style={{display: 'flex', alignItems: 'center'}}>
+                <img src={LogoCapaXCapa} style={{height: '2.986vw', width: '3.68vw', margin: '8px 2.986vw 9px 4.166vw'}}/>
+                <Link to={"/inicio"}> <button className="botones-Header">Inicio</button> </Link>
+                <Link to={"/nosotros"}> <button className="botones-Header">Nosotros</button> </Link>
+                <Link to={"/modelos"}> <button className="botones-Header">Modelos</button> </Link>
+                <Link to={"/contacto"}> <button className="botones-Header">Contacto</button> </Link>
             </div>
             {/* Íconos de e-mail e Instagram que se encuentran a la derecha respectivamente. */}
-            <div style={{marginLeft: 'auto', marginRight: '0.869vw'}}>
-                <Button onClick={() => window.location = 'mailto:capaxcapamty@gmail.com'} style={{width: '2.777vw'}}> <TbMail size="2.777vw"/> </Button>
-                <Button onClick={event => window.location.href='https://www.instagram.com/capaxcapa_oficial/'} style={{height: '2.272vw', width: '2.152vw'}}> <BsInstagram style={{height: '2.272vw', width: '2.152vw'}}/> </Button>
+            <div style={{marginLeft: 'auto', marginRight: '0.869vw', display: 'flex', alignItems: 'center'}}>
+                <button className="botones-iconos" onClick={() => window.location = 'mailto:capaxcapamty@gmail.com'} style={{marginRight: '0.5vw'}}> <TbMail size="2.777vw"/> </button>
+                <button className="botones-iconos" onClick={event => window.location.href='https://www.instagram.com/capaxcapa_oficial/'}> <BsInstagram style={{height: '2.272vw', width: '2.152vw'}}/> </button>
             </div>
         </div>
     )
