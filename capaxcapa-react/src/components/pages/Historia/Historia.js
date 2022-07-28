a31110b9b8196dc27c55446f4b05bb209611c422
Diego Vitales <diegorscorpion@gmail.com>
2022-07-28 16:20:21 -0500

    Cambios en imagen Nuestra Historia

--- capaxcapa-react/src/components/pages/Historia/Historia.js
+++ capaxcapa-react/src/components/pages/Historia/Historia.js
@@ -2,12 +2,13 @@ import './Historia.css';
 import ImagenFondo from '../../ImagenFondo.js'
 import Nacimiento from './Nacimiento.js';
 import Logros from './Logros.js';
+//import ImagenHeader from '../../../media/Images/Historia-Header.jpg'
 import ImagenHeader from '../../../media/Images/Historia-Header.jpg'
 
 const Historia = () => {
     return (
         <div>
-            <ImagenFondo imagen={ImagenHeader} titulo={"Nuestra Historia"} />
+            <ImagenFondo imagen={ImagenHeader} titulo={"Nuestra Historia"} colorTexto={'#D68B67'}/>
             <div className='font-face-cerebri-sans-book'>
                 <Nacimiento />
                 <div className="linea" style={{marginBottom: '5%'}}/>
